b463eb2a76464a698147c9ecb4dedac3c18caba5
Pedro Rezende <pedro.rezende@lbc.com.br>
2019-10-29 11:33:59 -0300

    init tray

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-const electron = require('electron');
+const path = require('path');
+const { app, Menu , Tray } = require('electron');
+
+app.dock.hide();
+
+app.on('ready', () => {
+    
+    const tray = new Tray(resolve(__dirname, 'image','icon.png'));
+    
+    const contextMenu = Menu.buildFromTemplate([
+        {label: 'Item1', type: 'radio', checked: true}
+    ]);
+    
+    tray.setContextMenu(contextMenu);
+    
+})
