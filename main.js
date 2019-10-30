8ccd7ceae0af80cbff0a33c90079fb9dcabca4a1
Pedro Rezende <pedro.rezende@lbc.com.br>
2019-10-30 12:26:50 -0300

    electron-store and fixing backupVersion

--- main.js
+++ main.js
@@ -5,10 +5,120 @@ const  AdmZip  = require ('adm-zip');
 
 
 
-const PATH_BACKUP = resolve(__dirname, 'backup', '');
-const FILE_PROCESS_BACKUP = 'lastversionTEMP.zip';
-const FILE_NAME = 'lastversion.zip';
+const PATH_BACKUP = resolve(__dirname, 'backup');
+const FILE_PROCESS_BACKUP = '\\lastversionTEMP.zip';
+const FILE_NAME = '\\lastversion.zip';
 const PATH_EXTRACT = resolve(__dirname);
+
+
+
+checkBackup = (pathBackup, filename) => {
+    
+
+    if (!fs.existsSync(pathBackup))
+      return false;    
+
+    if (!fs.existsSync(pathBackup  + filename))
+      return false;
+      
+    return true;  
+}
+
+backupVersion = (path, fileProcess, fileName) => {
+    try{
+
+        console.log(path);
+        // se existir um backup ele renomea temporariamente para fazer o processo
+        if(checkBackup(path, fileName)){
+            fs.rename(path + fileName, path + fileProcess); 
+        }
+
+        console.log("2");
+        
+        if(!fs.existsSync(path)){
+            fs.mkdirSync(path); 
+        }
+        
+
+        let zip = new AdmZip();
+
+        // colocar dentro de um repeat lista da raiz do sistema e uma lista de leitura com os arquivos da versão para comprar com a lista que esta pecorrendo 
+        const nameFileCurrent = "\\LbcDrbc.exe";
+
+        if(!fs.existsSync(path + nameFileCurrent)){
+            console.log("Arquivo nao encontrado!");
+            return;
+        }
+        
+        zip.addLocalFile(path + nameFileCurrent);
+        
+        console.log("4");
+        // compactado arquivos selecionado   
+        zip.writeZip(path + filename); 
+        console.log("5");
+        // deletando arquivo temporario do backup anterior
+        if(fs.exists(path + fileProcess)){               
+            fs.unlinkSync(path + fileProcess);              
+        }
+        console.log("6");
+
+    }catch{
+        // se o processo deu errado ele restaura o backup anterior
+        if(fs.exists(path + fileProcess)){
+            if(fs.exists(path + filename)){
+                fs.unlinkSync(path + filename);
+            }
+          
+            fs.rename(path + fileProcess, path + fileName);    
+                
+        }
+    
+    }
+   
+
+    
+}
+
+restoreVersion = () => {
+    
+}
+
+updateVersion = () => {
+
+    try{
+
+        if(this.checkBackup(path, filename)){
+            fs.rename(path + filename, path + fileProcess);
+        }
+        
+        if(!fs.exists(path))
+          fs.mkdir(path);
+
+          const zip = new AdmZip(path + filename);
+          const zipEntries = zip.getEntries();
+
+          zip.extractAllTo(resolve(__dirname), true);
+
+
+
+    }catch{
+        if(fs.exists(pathProcess)){
+            if(fs.exists(path))
+                fs.unlink(path);
+
+            fs.rename(pathProcess, path);    
+                
+        }
+          
+    }finally{
+
+    }
+}
+
+
+show = (mensage) => {
+    console.log(mensage);
+}
 
 
 
@@ -16,103 +126,11 @@ app.on('ready', () => {
     
     const tray = new Tray(resolve(__dirname, 'image','icon.png'));
 
-    checkBackup = (pathBackup, filename) => {
-        console.log(pathBackup + '/' + filename);
-
-        if (!fs.exists(pathBackup))
-          return false;
-
-        if (!fs.exists(pathBackup + '/' + filename))
-          return false;
-          
-        return true;  
-    }
-
-    backupVersion = (path, fileProcess, fileName) => {
-        try{
-            // se existir um backup ele renomea temporariamente para fazer o processo
-            if(this.checkBackup(path, fileName)){
-                fs.rename(path + fileName, path + fileProcess); 
-            }
-            
-            if(!fs.exists(path)){
-                fs.mkdir(path); 
-            }
-
-            let zip = new AdmZip();
-
-            // colocar dentro de um repeat lista da raiz do sistema e uma lista de leitura com os arquivos da versão para comprar com a lista que esta pecorrendo 
-            const nameFileCurrent = "LbcDrbc.exe";
-            zip.addLocalFile(path + nameFileCurrent);
-            
-            // compactado arquivos selecionado   
-            zip.writeZip(path + filename); 
-
-            // deletando arquivo temporario do backup anterior
-            if(fs.exists(path + fileProcess)){               
-                fs.unlink(path + fileProcess);              
-            }
-
-        }catch{
-            // se o processo deu errado ele restaura o backup anterior
-            if(fs.exists(path + fileProcess)){
-                if(fs.exists(path + filename)){
-                    fs.unlink(path + filename);
-                }
-              
-                fs.rename(path + fileProcess, path + fileName);    
-                    
-            }
-              
-        }finally{
-
-        }
-       
-
-        
-    }
-
-    restoreVersion = () => {
-        
-    }
-
-    updateVersion = () => {
-
-        try{
-
-            if(this.checkBackup(path, filename)){
-                fs.rename(path + filename, path + fileProcess);
-            }
-            
-            if(!fs.exists(path))
-              fs.mkdir(path);
-
-              const zip = new AdmZip(path + filename);
-              const zipEntries = zip.getEntries();
-
-              zip.extractAllTo(resolve(__dirname), true);
-
-
-
-        }catch{
-            if(fs.exists(pathProcess)){
-                if(fs.exists(path))
-                    fs.unlink(path);
-
-                fs.rename(pathProcess, path);    
-                    
-            }
-              
-        }finally{
-
-        }
-       
-
-    }
     
     const contextMenu = Menu.buildFromTemplate([
         {label: 'Back-Up', type: 'radio', checked: true, click: () => {
-            backupVersion(PATH_BACKUP, FILE_PROCESS_BACKUP, FILE_NAME);
+           backupVersion(PATH_BACKUP, FILE_PROCESS_BACKUP, FILE_NAME);
+           //show("Exemplo");
         }},
         {label: 'Atualizar', type: 'radio', checked: true, click: () =>{
             dialog.showOpenDialog({properties: ['openfile'] }, (paths) => {
