d4bb9dc254d4ce68b3d0137fe94a9b005b625909
Pedro Rezende <pedro.rezende@lbc.com.br>
2019-11-01 10:21:17 -0300

    update version

--- main.js
+++ main.js
@@ -3,127 +3,136 @@ const { app, Menu , Tray, dialog } = require('electron');
 const  fs  = require('fs');
 const  AdmZip  = require ('adm-zip');
 
-
-
+const PATH_RAIZ = resolve(__dirname, 'raiz');
+const PATH_DOWLOAD = resolve(__dirname, 'dowload');
 const PATH_BACKUP = resolve(__dirname, 'backup');
-const FILE_PROCESS_BACKUP = '\\lastversionTEMP.zip';
-const FILE_NAME = '\\lastversion.zip';
-const PATH_RAIZ = resolve(__dirname);
+const FILE_PROCESS_BACKUP = '\\backupversionTEMP.zip';
+const FILE_NAME = '\\backuplastversion.zip';
+const FIE_DOWLOAD = '\\lastversion.zip';
 
 
 
-checkBackup = (pathBackup, filename) => {
-    
+
+checkBackup = (pathBackup, fileName) => {
+    console.log("Checando backup existente...");
 
     if (!fs.existsSync(pathBackup))
       return false;    
 
-    if (!fs.existsSync(pathBackup  + filename))
+    if (!fs.existsSync(pathBackup  + fileName))
       return false;
       
     return true;  
 }
-roolBack = (path, fileProcess, filename) =>{
-    console.log("Roolback: " + path + fileProcess )
-    if(fs.existsSync(path + fileProcess)){
-        if(fs.existsSync(path + filename)){
-            fs.unlinkSync(path + filename);
-        }      
-        fs.renameSync(path + fileProcess, path + fileName); 
+roolBack = (path, fileProcess, fileName) =>{
+    console.log("Roolback: " + path + fileProcess );
+    if (checkBackup(path, fileName)){
+        if(fs.existsSync(path + fileProcess)){
+            if(fs.existsSync(path + fileName)){
+                fs.unlinkSync(path + fileName);
+            }      
+            fs.renameSync(path + fileProcess, path + fileName); 
+        }
     }
 }
 
-backupVersion = (raiz, path, fileProcess, fileName) => {
+backupVersion = (raiz, path, pathDowload, fileProcess, fileName, fileDowload) => {
     try{
 
-        console.log(path);
+        console.log('Caminho do back up: ' +path);
        
         // se existir um backup ele renomea temporariamente para fazer o processo
         if(checkBackup(path, fileName)){
             fs.renameSync(path + fileName, path + fileProcess); 
         }
 
-        console.log("2");
         
+        console.log("verificando diretorios");
         if(!fs.existsSync(path)){
             fs.mkdirSync(path); 
-        }
-        
+        }        
 
-        let zip = new AdmZip();
+        let zipBackup = new AdmZip();
+        // verifica se exist versão atualizada
+        console.log("verificando dowaload da ultima versão");
+        if (fs.existsSync(pathDowload + fileDowload)){
+            let zipDowload = new AdmZip(pathDowload + fileDowload);
 
-        // colocar dentro de um repeat lista da raiz do sistema e uma lista de leitura com os arquivos da versão para comprar com a lista que esta pecorrendo 
-        const nameFileCurrent = "\\LbcDrbc.exe";
+            // carregar zip em  memoria
+            let zipEntries = zipDowload.getEntries();           
+            console.log('Lendo arquivos de backup....');
+            zipEntries.forEach(function(zipEntry) {  
 
-        console.log(raiz + nameFileCurrent);
-        if(!fs.existsSync(raiz + nameFileCurrent)){
-            console.log("Arquivo nao encontrado!");
-            return;
-        }
-        
-        zip.addLocalFile(raiz + nameFileCurrent);
-       
-        console.log("4");
-        // compactado arquivos selecionado   
-        zip.writeZip(path + fileName, (error) =>{    
-            if (!error){
-                    console.log("5");
-                // deletando arquivo temporario do backup anterior
-                if(fs.existsSync(path + fileProcess)){               
-                    fs.unlinkSync(path + fileProcess);              
-                }
-                console.log("6");
-            }else{
-                // se o processo deu errado ele restaura o backup anterior
-                roolBack(path, fileProcess, fileName);               
-            }
-
-            console.log(error);
+                const nameFileCurrent = raiz + '\\' + zipEntry.entryName;
+                if (fs.existsSync(nameFileCurrent))    {
+                    zipBackup.addLocalFile(nameFileCurrent);
+                }               
+                    
             
+            });
+            console.log('compactando...');
+             // compactado arquivos selecionado 
+             zipBackup.writeZip(path + fileName, (error) =>{    
+                if (!error){    
+                            
+                    // deletando arquivo temporario do backup anterior
+                    if(fs.existsSync(path + fileProcess)){               
+                        fs.unlinkSync(path + fileProcess);              
+                    }
+                    console.log('sucesso');
 
-        }); 
-       
+                    return true;
+    
+                    
+                }else{                  
+                    // se o processo deu errado ele restaura o backup anterior
+                    roolBack(path, fileProcess, fileName);   
+                    console.log('Erro ao compactar arquivo de backup');
+                    return false;            
+                }
+            });
+            
+            return true;
+        }else{
+            console.log("Não foi possivel realizar backup! Ultima versão não foi baixada");
+            roolBack(path, fileProcess, fileName);
+            return false;            
+        }   
 
+        
+     
     }catch(error){
-        // se o processo deu errado ele restaura o backup anterior
-        console.log(error)
+        // se o processo deu errado ele restaura o backup anterior    
         roolBack(path, fileProcess, fileName);
+        console.log('false');
+        return false
     
     }
-   
-
-    
 }
 
-restoreVersion = () => {    
+restoreVersion = () => { 
+    console.log('Restaurando versao...')   
 }
 
-updateVersion = (pathRaiz, pathBackup, fileName, FileNameBackup) => {
+updateVersion = (raiz, path, pathDowload, fileProcess, fileName, fileDowload) => {
 
     try{
+        if (!backupVersion(raiz, path, pathDowload, fileProcess, fileName, fileDowload))
+          throw "Erro ao realizar backup";
 
-        if(this.checkBackup(path, filename)){
-            fs.renameSync(path + filename, path + fileProcess);
-        }
+       
         
-        if(!fs.existsSync(path))
-          fs.mkdirSync(path);
-
-          const zip = new AdmZip(path + filename);
-          const zipEntries = zip.getEntries();
-
-          zip.extractAllTo(resolve(__dirname), true);
-
-
+        if(!fs.existsSync(pathDowload))
+          fs.mkdirSync(pathDowload);
+        
+        console.log('Extraindo versao...');
+        const zip = new AdmZip(pathDowload + fileDowload);  
+        zip.extractAllTo(raiz, true);
+        
+        console.log('Versao extraida com sucesso!');
 
     }catch{
-        if(fs.exists(pathProcess)){
-            if(fs.exists(path))
-                fs.unlink(path);
-
-            fs.rename(pathProcess, path);    
-                
-        }
+       restoreVersion();
           
     }finally{
 
@@ -144,13 +153,11 @@ app.on('ready', () => {
     
     const contextMenu = Menu.buildFromTemplate([
         {label: 'Back-Up', type: 'radio', checked: true, click: () => {
-           backupVersion(PATH_RAIZ, PATH_BACKUP, FILE_PROCESS_BACKUP, FILE_NAME);
+           backupVersion(PATH_RAIZ, PATH_BACKUP, PATH_DOWLOAD, FILE_PROCESS_BACKUP, FILE_NAME, FIE_DOWLOAD);
            //show("Exemplo");
         }},
         {label: 'Atualizar', type: 'radio', checked: true, click: () =>{
-            dialog.showOpenDialog({properties: ['openfile'] }, (paths) => {
-                console.log(paths);     
-            });
+            updateVersion(PATH_RAIZ, PATH_BACKUP, PATH_DOWLOAD, FILE_PROCESS_BACKUP, FILE_NAME, FIE_DOWLOAD);
         }},
         {label: 'Restore', type: 'radio', checked: true, click: () =>{
             dialog.showOpenDialog({properties: ['openfile'] }, (paths) => {
