f50bd6fb783c33e085c40cdbaf9ab88f8ef82e62
Fernando Freitas <fernando@lbc.com.br>
2019-11-01 14:02:37 -0300

    merger pacial client

--- main/helpers/index.ts
+++ main/helpers/index.ts
@@ -1,9 +1,13 @@
 import createWindow from './create-window';
 import exitOnChange from './exit-on-change';
 import backupVersion from './backup-version';
+import updateVersion from './updade-version';
+import restoreVersion from './restore-version';
 
 export {
   createWindow,
   exitOnChange,
   backupVersion,
+  updateVersion,
+  restoreVersion,
 };
--- main/helpers/updade-version.ts
+++ main/helpers/updade-version.ts
@@ -1,3 +1,27 @@
-export default function updadeVersion(): void {
+import backupVersion from './backup-version';
+import restoreVersion from './restore-version';
+const fs = require('fs');
+const AdmZip = require('adm-zip');
+
+export default function updadeVersion(raiz, path, pathDowload, fileProcess, fileName, fileDowload): void {
+    try {
+        if (!backupVersion(raiz, path, pathDowload, fileProcess, fileName, fileDowload))
+            throw "Erro ao realizar backup";
+
+        if (!fs.existsSync(pathDowload))
+            fs.mkdirSync(pathDowload);
+
+        console.log('Extraindo versao...');
+        const zip = new AdmZip(pathDowload + fileDowload);
+        zip.extractAllTo(raiz, true);
+
+        console.log('Versao extraida com sucesso!');
+
+    } catch{
+        restoreVersion();
+
+    } finally {
+
+    }
 
 }
